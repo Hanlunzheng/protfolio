a3a80528330c1f8b50b08643fdd10f000fe04235
Hanlun Zheng <hanlunzheng@macbook-pro-3.mynetworksettings.com>
2024-02-07 04:34:34 -0500

    hey

--- src/data/data.js
+++ src/data/data.js
@@ -7,45 +7,53 @@ import social from "../assets/projects/blogpage.png";
 import bank from "../assets/projects/backTracker.png";
 import movie from "../assets/projects/moviesRating.png"
 // import Fiverr from "../assets/projects/";
+import wordpress from "../assets/projects/wordpress.png"
 
 export const data = [
   {
     id: 1,
+    name: "WordPress",
+    image: wordpress,
+    github: "https://github.com/Hanlunzheng",
+    live: "https://aaronfangtry.com/",
+  },
+  {
+    id: 2,
     name: "Food delivery Application",
     image: food,
     github: "https://github.com/Hanlunzheng/Food-delivery-",
     live: "https://food-delivery-rose.vercel.app/",
   },
   {
-    id: 2,
+    id: 3,
     name: "Commercial landing page",
     image: Commercial,
     github: "https://github.com/Hanlunzheng/commerciallist/tree/main",
     live: "https://hanlunzheng.github.io/commerciallist/",
   },
+  // {
+  //   id: 4,
+  //   name: "Social media app",
+  //   image: social,
+  //   github: "https://github.com/Hanlunzheng/social-media-app",
+  //   live: "http://social-media-qu3uxed63-hanlunzhengs-projects.vercel.app",
+  // },
   {
-    id: 3,
-    name: "Social media app",
-    image: social,
-    github: "https://github.com/Hanlunzheng/social-media-app",
-    live: "http://social-media-qu3uxed63-hanlunzhengs-projects.vercel.app",
-  },
-  {
-    id: 4,
+    id: 5,
     name: "food page",
     image: foodpage,
     github: "https://github.com/Hanlunzheng/foodpage",
     live: "https://hanlunzheng.github.io/foodpage/",
   },
   {
-    id: 5,
+    id: 6,
     name: "Bank tracking demo",
     image: bank,
     github: "https://github.com/Hanlunzheng/bankAccount-tracker",
     live: "https://expense-tracker-70b1a.web.app",
   },
   {
-    id: 6,
+    id: 7,
     name: "Fetching data from API",
     image: movie,
     github: "https://github.com/Hanlunzheng/Movierating",
